16d7d569e46b879ee6504ab7c51a5d3ae8362db0
valentin <valentin@suruceanu.net>
2023-03-01 18:28:11 +0200

    minor presentation changes, and index.js changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-const container = document.getElementById('container');
 
-const noteContainer = document.createElement('div');
-noteContainer.className = 'note-container';
+function createInputNote() {
+    const container = document.getElementById('container');
 
-const noteTitle = document.createElement('input');
-noteTitle.className = 'note-title-input';
-noteTitle.placeholder = 'Add note title';
+    const noteContainer = document.createElement('div');
+    noteContainer.className = 'note-container';
 
-const noteText = document.createElement('textarea');
-noteText.className = 'note-text-input';
-noteText.placeholder = 'Add note text...';
+    const noteTitle = document.createElement('input');
+    noteTitle.className = 'note-title-input';
+    noteTitle.placeholder = 'Add note title';
 
-const removeNoteBtn = document.createElement('button');
-removeNoteBtn.className = 'note-add-btn';
-removeNoteBtn.textContent = '+';
-removeNoteBtn.addEventListener('click', addChild);
+    const noteText = document.createElement('textarea');
+    noteText.className = 'note-text-input';
+    noteText.placeholder = 'Add note text...';
 
-noteContainer.appendChild(noteTitle);
-noteContainer.appendChild(noteText);
-noteContainer.appendChild(removeNoteBtn);
-container.appendChild(noteContainer);
+    const removeNoteBtn = document.createElement('button');
+    removeNoteBtn.className = 'note-add-btn';
+    removeNoteBtn.textContent = '+';
+    removeNoteBtn.addEventListener('click', addChild);
+
+    noteContainer.appendChild(noteTitle);
+    noteContainer.appendChild(noteText);
+    noteContainer.appendChild(removeNoteBtn);
+    container.appendChild(noteContainer);
+}
 
 function addChild(e) {
     const titleValue = e.target.parentNode.getElementsByClassName('note-title-input').item(0);
